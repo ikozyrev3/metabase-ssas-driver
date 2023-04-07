278612b949aedb1027eb40d310ae50847fb36d73
rocky57 <piotr.rokosz88@gmail.com>
2023-04-07 09:48:36 +0200

    test

--- project.clj
+++ project.clj
@@ -3,7 +3,10 @@
   :dependencies [
 					[org.clojure/clojure "1.10.1"]
 					[org.olap4j/olap4j "1.2.0"]
-					[org.olap4j/olap4j-xmla "1.2.0-JS-1"]
+					[org.olap4j/olap4j-xmla "1.1.0"]
 				 ]
-  :repositories {"jaspersoft" "https://jaspersoft.jfrog.io/artifactory/repo/"}
-)
+  :repositories {
+		"jaspersoft" {:url "https://jaspersoft.jfrog.io/ui/native/jrs-ce-addons-releases/org/olap4j/olap4j-xmla/1.1.0"}
+		"mvnrepository" {:url "https://mvnrepository.com/"}
+		"jboss" {:url "https://repository.jboss.org/"}
+})
